ecc85539ff61a05fadc43a8c2d2f5ea6b6fd1b2c
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2024-04-08 19:08:17 -0600

    added code relating to button scaling and rotation

--- script.js
+++ script.js
@@ -2,29 +2,42 @@ const specialBtn = document.querySelector(".btn-special");
 const greenBtn = document.querySelector(".btn-green");
 const redYellowBtn = document.querySelector(".btn-red-yellow");
 const oddBtn = document.querySelector(".btn-odd");
-const nonBlackBtn = document.querySelector(".btn-non-black");
-const spinBtn = document.querySelector(".btn-spin");
+const notBlackBtn = document.querySelector(".btn-not-black");
+const rotateAllBtn = document.querySelector(".btn-rotate-all");
 
 /*Animate logic*/
 var special = anime({
   targets: document.getElementById("special"),
   translateY: 250,
+  autoplay: false,
 });
 var firstGreen = anime({
   targets: document.querySelector(".green"),
   translateY: 250,
+  autoplay: false,
 });
 var redYellow = anime({
   targets: document.querySelectorAll(".red, .yellow"),
   translateY: 250,
+  autoplay: false,
 });
 var odd = anime({
   targets: document.querySelectorAll(".square:nth-child(odd)"),
   translateY: 250,
+  autoplay: false,
 });
 var notBlack = anime({
   targets: document.querySelectorAll(".square:not(.black)"),
   translateY: 250,
+  autoplay: false,
+});
+var rotateAll = anime({
+  targets: document.querySelectorAll(".square"),
+  rotateY: "360",
+  easing: "linear",
+  loop: true,
+  duration: 800,
+  autoplay: false,
 });
 
 /*Add Event Listeners*/
@@ -32,4 +45,5 @@ specialBtn.addEventListener("click", special);
 greenBtn.addEventListener("click", firstGreen);
 redYellowBtn.addEventListener("click", redYellow);
 oddBtn.addEventListener("click", odd);
-nonBlackBtn.addEventListener("click", notBlack);
+notBlackBtn.addEventListener("click", notBlack);
+rotateAllBtn.addEventListener("click", rotateAll);
