d427fec4e27c76113de338c422a8d4b03766d06a
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2024-04-08 19:46:28 -0600

    added design code

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+/*Necessary variables*/
 const specialBtn = document.querySelector(".btn-special");
 const greenBtn = document.querySelector(".btn-green");
 const redYellowBtn = document.querySelector(".btn-red-yellow");
 const oddBtn = document.querySelector(".btn-odd");
 const notBlackBtn = document.querySelector(".btn-not-black");
+const turbulenceAllBtn = document.querySelector(".btn-turbulence-all");
 const rotateAllBtn = document.querySelector(".btn-rotate-all");
 
 /*Animate logic*/
@@ -31,6 +33,15 @@ var notBlack = anime({
   translateY: 250,
   autoplay: false,
 });
+var turbulenceAll = anime({
+  targets: document.querySelectorAll(".square"),
+  points: "64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96",
+  baseFrequency: 0,
+  scale: 1,
+  loop: true,
+  direction: "alternate",
+  easing: "easeInOutExpo",
+});
 var rotateAll = anime({
   targets: document.querySelectorAll(".square"),
   rotateY: "360",
@@ -40,7 +51,7 @@ var rotateAll = anime({
   autoplay: false,
 });
 
-/*Add Event Listeners*/
+/*Hook Up Event Listeners*/
 specialBtn.addEventListener("click", special);
 greenBtn.addEventListener("click", firstGreen);
 redYellowBtn.addEventListener("click", redYellow);
